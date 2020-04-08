8846fbf09c4f6b0ee2be200b17a78c672bd1f28d
MareikeJaniak <mareike.janiak@gmail.com>
2020-04-08 13:37:16 -0600

    rename tree

--- Scripts/ACE2_gene_tree_RAxML.sh
+++ Scripts/ACE2_gene_tree_RAxML.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 raxmlHPC -f a -m GTRGAMMA -p 12345 -x 12345 -# 1000 -s ../Alignments/ACE2_nucleotide_alignment_with_outgroup.fasta -n ACE2_RAxML_tree
+
+# rename best tree with support values and keep in GeneTrees directory
+cp RAxML_bipartitions.ACE2_RAxML_tree ../GeneTrees/ACE2_RAxML_tree.newick
